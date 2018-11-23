4430018fda9625a08c34f2ef563586a22fc3ddb8
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2018-11-23 20:53:35 +0000

    basic audience menu functionality

--- audience-app/src/app/app.component.ts
+++ audience-app/src/app/app.component.ts
@@ -11,9 +11,13 @@ import { CurrentState, Configuration, MenuItem, View } from './types';
 export class AppComponent {
   loading:boolean = true
   currentState:CurrentState = null
-  menuItems:MenuItem[]
+  allMenuItems:MenuItem[]
+  menuItems:MenuItem[] = []
   views:View[]
-
+  showMenu:boolean = false
+  forceView:string
+  showMenuItem
+  
   constructor(
     private syncService:SyncService,
   ) 
@@ -21,13 +25,29 @@ export class AppComponent {
     syncService.getConfiguration().subscribe((configuration) => {
       if (!configuration)
         return
-      this.menuItems = configuration.menuItems
+      this.allMenuItems = configuration.menuItems
       this.views = configuration.views
     })
     syncService.getCurrentState().subscribe((newState) => {
       console.log('home update current state', newState)
       this.currentState = newState
       this.loading = !this.currentState
+      if (this.currentState && this.currentState.allowMenu && !this.forceView) {
+        this.showMenu = true
+      }
+      if (this.allMenuItems) {
+        this.menuItems = this.allMenuItems.filter((item) => (this.currentState && this.currentState.postPerformance) || !item.postPerformance)
+      }
     })
+  }
+  onShowMenuItem(menuItem:MenuItem):void {
+    console.log(`show menu item`, menuItem)
+    this.showMenu = false
+    this.showMenuItem = menuItem
+  }
+  onShowMenu():void {
+    console.log('show menu')
+    this.showMenu = true
+    this.showMenuItem = null
   }
 }
