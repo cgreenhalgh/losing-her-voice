9e185d80fe67ed6f494f1fe9fda7bbe5819c901b
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2019-06-17 16:46:43 +0100

    logtools initial client event list

--- logtools/src/clients.ts
+++ logtools/src/clients.ts
@@ -1,6 +1,7 @@
 // LHV clients.ts
 
 import * as fs from 'fs' 
+import * as dateFormat from 'dateformat'
 
 if (process.argv.length < 3) {
   console.log("usage: node dist/clients.js <audience-logfile> ...")
@@ -14,6 +15,7 @@ for (let i=2; i<process.argv.length; i++) {
 interface ClientEvent {
 	msg:string
 	time:string
+	info?:any
 }
   	
 interface LogEntry {
@@ -31,10 +33,35 @@ interface ClientLogEntry extends LogEntry {
   events:ClientEvent[]
 }
 enum Msg {
+	LOAD = "load",
+	TAPTOSTART = "taptostart",
+	PATH = "path",
+	ITEM = "item",
+	VIEW = "view",
+	NOVIEW = "noview",
+	VISIBLE = "visible",
+	HIDDEN = "hidden",
 	CLIENT_LOG = "client.log",
 	TAKESELFIE = "takeselfie",
-	CONSENT2 = "consent2"
+	CONSENT2 = "consent2",
+	PLAY = "play"
 }
+let userActions:string[] = [
+  Msg.PATH, //? not alwyas
+  Msg.VISIBLE, //duplicates?!
+  Msg.HIDDEN,
+  "name",
+  Msg.TAKESELFIE, 
+  "consent1", 
+  Msg.CONSENT2,
+  Msg.TAPTOSTART,
+  "link",
+  "like",
+  "share",
+  "choose.select",
+  "choose",
+  "clearuserdata"
+]
 
 let entries:LogEntry[] = []
 
@@ -69,6 +96,16 @@ class Client {
 	takeselfieTime:number
 	consent2:boolean
 	consent2Time:number
+	onloadscreen?:boolean
+	visible?:boolean
+	visibleTime?:number
+  viewId?:string
+	viewShowItems?:boolean
+  noviewTime?:number
+	path?:string
+	pathShowItems?:boolean // path '/.../posts'
+	itemId?:string
+  itemType?:string
 }
 
 interface ClientMap {
@@ -123,6 +160,9 @@ for (let entry of entries) {
 
 const PRE_SHOW_TIME_MS = 1*60*60*1000 // 1 hour
 const SHOW_TIME_MS = 3*60*60*1000 // 3 hours (including interval and some time after)
+const TIME_FORMAT = 'H:MM:ss'
+const FIRST_PATH_TIME_MS = 300
+const SHOW_HIDDEN_EVENTS = false
 
 for (let performanceId in performances) {
 	console.log(`*** Performance ${performanceId} ***`)
@@ -162,4 +202,97 @@ for (let performanceId in performances) {
 	}
 	console.log(`performance ${performanceId}, ${pclients.length} clients`)
 	console.log(`takeselfies: ${pclients.filter((client)=>client.takeselfie).length}, consent2 ${pclients.filter((client)=>client.consent2).length}`)
+  for (let client of pclients) {
+    console.log(`- client ${client.clientId} (performance ${client.performanceId}):`)
+    client.events.sort(sortbytime)
+    
+    for (let event of client.events) {
+    	let time = new Date(event.time).getTime()
+    	let visibleChange = false
+    	let userAction = userActions.indexOf(event.msg) >= 0
+    	let hiddenUserAction = false
+    	if (event.msg == Msg.LOAD) {
+    		if (!client.onloadscreen)
+    			visibleChange = true
+    		client.onloadscreen = true
+    	} else if (event.msg == Msg.TAPTOSTART) {
+    		client.onloadscreen = false
+    		visibleChange = true
+    	}
+    	else if (event.msg == Msg.PATH) {
+    		if (!client.onloadscreen && !client.viewId && client.path != event.info.path)
+    			visibleChange = true
+    		// initial path
+    		if (client.visibleTime && time < client.visibleTime + FIRST_PATH_TIME_MS) {
+    			userAction = false
+    			client.visibleTime = 0
+    		}
+    		// auto-path after act
+    		if (client.noviewTime && time < client.noviewTime + FIRST_PATH_TIME_MS) {
+    			userAction = false
+    			client.noviewTime = 0
+    		} 		
+    	  // default paths
+    	  if ((!client.path && event.info.path == '/'+client.performanceId) || (client.path == '/'+client.performanceId && event.info.path == '/'+client.performanceId+'/home'))
+    	  	userAction = false
+    		client.path = event.info.path
+    		client.pathShowItems = new RegExp("^\/[^\/]*\/posts$").test(client.path)
+    	} else if (event.msg == Msg.ITEM) {
+    		if (!client.onloadscreen && client.itemId != event.info.id && ((client.viewId && client.viewShowItems) || (!client.viewId && client.pathShowItems)))
+    			visibleChange = true
+    		client.itemId = event.info.id
+    		client.itemType = event.info.itemType
+    	} else if (event.msg == Msg.VIEW) {
+    		if (!client.onloadscreen && client.viewId != event.info.id)
+    			visibleChange = true
+    		client.viewId = event.info.id
+    		client.viewShowItems = event.info.showItems
+    	} else if (event.msg == Msg.NOVIEW) {
+    		if (!client.onloadscreen && !client.viewId)
+    			visibleChange = true
+    		client.viewId = null
+    		client.viewShowItems = false
+    		client.noviewTime = time
+    	} else if (event.msg == Msg.PLAY) {
+    		if (client.visible || client.onloadscreen)
+    			visibleChange = true
+    	}
+    	
+    	if (event.msg == Msg.VISIBLE) {
+    		visibleChange = !client.visible
+    		userAction = !client.visible
+    		client.visible = true
+    		client.visibleTime = new Date(event.time).getTime()
+    	}
+    	else if (event.msg == Msg.HIDDEN) {
+    		visibleChange = client.visible
+    	} else if (userAction && !client.visible) {
+    		hiddenUserAction = true    		
+    	}
+    	
+    	let date = event.time
+    	let delta = new Date(event.time).getTime() - startTime
+    	if (delta> -60*60*1000 && delta<0)
+    		date = '-'+dateFormat(new Date(-delta), TIME_FORMAT)
+    	else if (delta>=0 && delta< 24*60*60*1000)
+    		date = '+'+dateFormat(new Date(delta), TIME_FORMAT)
+      
+    		
+      if (!hiddenUserAction && !userAction && (!client.visible || !visibleChange)) {
+      	// remove?
+      	if (SHOW_HIDDEN_EVENTS)
+         	console.log(`  [${date} ${event.msg} ${event.info ? JSON.stringify(event.info) : ''}]`)
+      } else {
+        let state = client.onloadscreen ? 'loadscreen' : (client.viewId ? `view ${client.viewId}` : `page ${client.path}`)
+     	  let showItems = client.viewId ? client.viewShowItems : client.pathShowItems
+     	  if (showItems)
+     		  state = state + ` item ${client.itemType} ${client.itemId}`
+     		let showInfo = event.info && event.msg != Msg.VIEW && event.msg != Msg.PATH && event.msg != Msg.ITEM
+    	  console.log(`${hiddenUserAction ? '?' : (userAction ? '*' : ' ')} ${date} ${event.msg} ${state} ${showInfo ? JSON.stringify(event.info) : ''}`)
+    	  if (event.msg == Msg.HIDDEN) {
+     	  	client.visible = false
+     	  }     
+      }
+    }
+  }
 }
